e2aa5a34fbfc56ce8cb858a93b440d7e1b49d725
Dinesh N K <nkdweb0@gmail.com>
2023-07-30 22:56:30 +0530

    Updated hydrate & render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
@@ -8,12 +8,22 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
+if (rootElement.hasChildNodes()) {
+  root.hydrate(<StrictMode>
     <HelmetProvider>
       <Router>
         <App />
       </Router>
     </HelmetProvider>
-  </StrictMode>
-);
+  </StrictMode>);
+} else {
+  root.render(
+    <StrictMode>
+      <HelmetProvider>
+        <Router>
+          <App />
+        </Router>
+      </HelmetProvider>
+    </StrictMode>);
+}
+
